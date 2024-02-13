c212b671ce604d93b9eefe22413e65f2c1017e4c
Andrea Cabrera <3774322450101@ingenieria.usac.edu.gt>
2024-02-13 16:43:38 -0600

    Endpoint Create Product

--- Backend/src/controllers/products.controller.js
+++ Backend/src/controllers/products.controller.js
@@ -1,9 +1,32 @@
-import productModel from "../db/models/product.model";
+import Product from "../db/models/product.model.js";
 
 export const createProduct = async (req, res) => {
     try {
-        const {pathImage, name, description, price, stock, idUser} = req.body;
-        
+        const { pathImage, name, description, price, stock, idUser } = req.body;
+        console.log(req.body);
+        if (!pathImage || !name || !description || !price || !stock || !idUser) {
+            return res.response(null, "All fields are required", 400);
+        }
+
+        if (typeof price !== "number" || typeof stock !== "number") {
+            return res.response(null, "Price and stock must be numbers", 400);
+        }
+
+        if (price <= 0 ) {
+            return res.response(null, "Price must be greater than 0", 400);
+        }
+
+        Product.create({
+            pathImage: pathImage,
+            name: name,
+            description: description,
+            price: price,
+            stock: stock,
+            idUser: idUser
+        })
+        res.response(null, "Product created successfully", 200);
+
+
     } catch (error) {
         console.error(error);
         res.response(null, error.message, 500);
--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
+import { createProduct } from '../controllers/products.controller.js';
 
 const router = Router();
 
 router.post('/create', createProduct);
-
 
 export default router;
