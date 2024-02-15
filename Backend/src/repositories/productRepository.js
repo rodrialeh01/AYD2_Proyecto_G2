4493203298e5f9ae1fe81ee2a59cc49436405225
Andrea Cabrera <3774322450101@ingenieria.usac.edu.gt>
2024-02-14 21:29:50 -0600

    Update Patrón Repository y update

--- Backend/src/repositories/productRepository.js
+++ Backend/src/repositories/productRepository.js
@@ -1,8 +1,24 @@
 import Product from "../db/models/product.model.js";
+import validator from "validator";
 
 class ProductRepository {
     async obtenerTodos() {
         return await Product.find({});
+    }
+
+    async crearProducto(producto) {
+        try {
+            return await Product.create(producto);
+        } catch (error) {
+            console.error(error);
+            return res.response(null, error.message, 500);
+        }
+    }
+
+    async updateProduct(id, producto) {
+    
+        return await Product.findByIdAndUpdate(id, producto, { new: true });
+
     }
 }
 
